fc1677267402c02adbd4995136401edde5528408
Chris Son <cson@wrapbook.com>
2024-12-20 15:07:44 -0800

    linting

--- app/controllers/income_forecast_controller.rb
+++ app/controllers/income_forecast_controller.rb
@@ -20,9 +20,10 @@ class IncomeForecastController < ApplicationController
 
   def set_currencies
     # TODO: Setting default currencies for now.
-    @base_currency = currency_params[:base_currency] || "CAD"
-    @quote_currency = currency_params[:quote_currency] || "USD"
-    @currency_service = CurrencyService.new(base_currency: @base_currency, quote_currency: @quote_currency)
+    @base_currency = currency_params[:base_currency] || 'CAD'
+    @quote_currency = currency_params[:quote_currency] || 'USD'
+    @currency_service = CurrencyService.new(base_currency: @base_currency,
+                                            quote_currency: @quote_currency)
     @conversion_rate = @currency_service.conversion_rate
   end
 
@@ -33,7 +34,7 @@ class IncomeForecastController < ApplicationController
       conversion_rate: @conversion_rate,
       business_days: BusinessDaysCalculator.remaining_days_in_year
     )
-    
+
     @projected_remaining_income = @income_calculator.base_income
     @projected_remaining_income_in_quote_currency = @income_calculator.converted_income
   end
