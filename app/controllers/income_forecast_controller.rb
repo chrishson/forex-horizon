8d415d0fc62b2ae91c42bcc48dd4f9cb2b043923
Chris Son <cson@wrapbook.com>
2024-12-20 15:00:41 -0800

    Added dev configs and basic projected income

--- app/controllers/income_forecast_controller.rb
+++ app/controllers/income_forecast_controller.rb
@@ -1,27 +1,48 @@
-require 'httparty'
-
+# app/controllers/income_forecast_controller.rb
 class IncomeForecastController < ApplicationController
-  def index
-    # Replace this URL with your API endpoint
+  before_action :set_currencies
+  before_action :set_projected_income, only: %i[index create]
 
-    # Using: https://www.exchangerate-api.com/docs/standard-requests
-    # url = URI("#{ENV['EXCHANGE_RATE_API_URL']}/CAD")
-    #
-    # response = HTTParty.get(url)
-    #
-    # @api_data = response.parsed_response
+  def index; end
 
-    # Faking API calls for now. 1500 a month limit.
-    @api_data = {
-      "conversion_rates" => {
-        "USD" => 0.75
-      }
-    }
+  def create
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          'projected_income',
+          partial: 'income_forecast/projected_income'
+        )
+      end
+    end
+  end
 
-    # TODO: hard coding USD for now until forms implemented.
-    @quote_currency = "USD"
-    @base_to_quote_conversion_rate = @api_data.dig("conversion_rates", @quote_currency)
-  rescue StandardError => e
-    @error_message = "Error fetching API data: #{e.message}"
+  private
+
+  def set_currencies
+    # TODO: Setting default currencies for now.
+    @base_currency = currency_params[:base_currency] || "CAD"
+    @quote_currency = currency_params[:quote_currency] || "USD"
+    @currency_service = CurrencyService.new(base_currency: @base_currency, quote_currency: @quote_currency)
+    @conversion_rate = @currency_service.conversion_rate
+  end
+
+  def set_projected_income
+    @income_calculator = IncomeCalculator.new(
+      hours_per_day: income_params[:hours_per_day],
+      hourly_rate: income_params[:hourly_rate_dollars],
+      conversion_rate: @conversion_rate,
+      business_days: BusinessDaysCalculator.remaining_days_in_year
+    )
+    
+    @projected_remaining_income = @income_calculator.base_income
+    @projected_remaining_income_in_quote_currency = @income_calculator.converted_income
+  end
+
+  def currency_params
+    params.permit(:base_currency, :quote_currency)
+  end
+
+  def income_params
+    params.permit(:hours_per_day, :hourly_rate_dollars)
   end
 end
---
+++ app/services/business_days_calculator.rb
@@ -0,0 +1,16 @@
+class BusinessDaysCalculator
+  def self.remaining_days_in_year
+    today = Date.today
+    end_of_year = Date.new(today.year, 12, 31)
+    
+    (today..end_of_year).count do |date|
+      !weekend?(date)
+    end
+  end
+
+  private
+
+  def self.weekend?(date)
+    date.saturday? || date.sunday?
+  end
+end
