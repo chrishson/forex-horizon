eea329935ec9b4032039f7f5067b38f6d582c5a6
Chris Son <cson@wrapbook.com>
2024-12-22 14:10:07 -0800

    fixed bug with quote and base currency

--- app/controllers/income_forecast_controller.rb
+++ app/controllers/income_forecast_controller.rb
@@ -20,8 +20,11 @@ class IncomeForecastController < ApplicationController
             'projected_income',
             partial: 'income_forecast/projected_income',
             locals: {
-              projected_remaining_income: @projected_remaining_income,
-              projected_remaining_income_in_quote_currency: @projected_remaining_income_in_quote_currency
+              base_income: @base_income,
+              quote_income: @quote_income,
+              business_days_remaining: @business_days_remaining,
+              weekend_days_remaining: @weekend_days_remaining,
+              total_days_remaining: @total_days_remaining
             }
           )
         ]
@@ -59,18 +62,28 @@ class IncomeForecastController < ApplicationController
   end
 
   def set_projected_income
+    # TODO: Not really just projected_income, refactor name and what it does later.
     return if @conversion_rate.blank?
+
+    days_remaining_service = DaysRemaining.new(
+      start_date: form_params[:start_date].to_date,
+      end_date: form_params[:end_date].to_date
+    )
+
+    @business_days_remaining = days_remaining_service.business_days_remaining
+    @weekend_days_remaining = days_remaining_service.weekend_days_remaining
+    @total_days_remaining = days_remaining_service.total_days_remaining
 
     @income_calculator = IncomeCalculator.new(
       hours_per_day: form_params[:hours_per_day],
-      hourly_rate: form_params[:hourly_rate_dollars],
+      hourly_rate: form_params[:hourly_rate],
       conversion_rate: form_params[:conversion_rate],
-      business_days: BusinessDaysCalculator.remaining_days_in_year
+      business_days: @business_days_remaining
     )
 
     @errors = []
-    @projected_remaining_income = @income_calculator.base_income
-    @projected_remaining_income_in_quote_currency = @income_calculator.converted_income
+    @base_income = @income_calculator.base_income
+    @quote_income = @income_calculator.quote_income
   end
 
   def currency_params
@@ -78,15 +91,13 @@ class IncomeForecastController < ApplicationController
   end
 
   def form_params
-    params.permit(:hours_per_day, :hourly_rate_dollars, :conversion_rate)
+    params.permit(:hours_per_day, :hourly_rate, :conversion_rate, :start_date, :end_date)
   end
 
   def validate_form_params
     @errors = []
     form_params.each do |key, value|
-      if value.blank? || value.to_f <= 0
-        @errors << "#{key} must be a positive number"
-      end
+      @errors << "#{key} must be a positive number" if value.blank? || value.to_f <= 0
     end
   end
 end
