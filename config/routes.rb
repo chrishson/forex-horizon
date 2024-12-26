9077b3924a8f97706d4ed9cd3a497049f8704b03
Chris Son <cson@wrapbook.com>
2024-12-26 15:22:06 -0800

    refactored controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  resources :income_forecast, path: 'income-forecast', only: [:index] do
-    collection do
-      post :update_forecasted_income, path: 'update-forecasted-income'
-      post :update_conversion_rate, path: 'update-conversion-rate'
+  get 'income-forecast' => 'income_forecast#index', as: :income_forecast
+
+  namespace :forms do
+    resources :income_forecast_form, path: 'income-forecast-form', only: [:index] do
+      collection do
+        post :update_forecasted_income, path: 'update-forecasted-income'
+      end
+    end
+    resources :currency_conversion_form, path: 'currency-conversion-form', only: [:index] do
+      collection do
+        post :update_conversion_rate, path: 'update-conversion-rate'
+      end
     end
   end
 end
